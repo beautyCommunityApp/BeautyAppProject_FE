f970694a7aae7d81677b4067f86c74ff7ca3c3d6
yujin070 <sonyumust1@gmail.com>
2025-06-13 14:08:03 +0900

    아티클 부분 수정

--- src/pages/Article/ArticleDetail.jsx
+++ src/pages/Article/ArticleDetail.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchArticleDetail } from "../../api/articleApi";
+import { toggleArticleLike } from "../../api/articleApi";
+
 import "./ArticleDetail.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+
+import offHeart from "../../assets/images/off_h.png";
+import onHeart from "../../assets/images/on_h.png";
 
 function ArticleDetail() {
   const { articleId } = useParams();
@@ -33,6 +38,18 @@ function ArticleDetail() {
 
   if (!article) return <div>Loading...</div>;
 
+  const handleToggleLike = async () => {
+    try {
+      const res = await toggleArticleLike(article.articleId);
+      setArticle((prev) => ({
+        ...prev,
+        isLiked: res.result.isLiked,
+        likeCount: res.result.likeCount,
+      }));
+    } catch (err) {
+      console.error("좋아요 토글 실패", err);
+    }
+  };
   return (
     <>
       <Header />
@@ -50,7 +67,9 @@ function ArticleDetail() {
           </div>
 
           <p className="article-body">{article.articleContent}</p>
-
+          {article.daysAgo && (
+            <p className="article-days-ago">{article.daysAgo}</p>
+          )}
           {/* 에디터 작성자 부분  아티클 등록한 대상자 */}
           {article.editor && (
             <div className="article-editor">
@@ -67,7 +86,7 @@ function ArticleDetail() {
           )}
 
           {/* 수정 후 */}
-          {/* {article.writerName && (
+          {article.writerName && (
             <div className="article-editor">
               <img
                 src="/dummy/editor.png" // 고정값 또는 기본 이미지
@@ -79,7 +98,7 @@ function ArticleDetail() {
                 <div className="editor-name">EDITOR {article.writerName}</div>
               </div>
             </div>
-          )} */}
+          )}
 
           {article.comments && article.comments.length > 0 && (
             <div className="article-comments">
@@ -95,21 +114,64 @@ function ArticleDetail() {
                       className="comment-profile"
                     />
                     <div className="comment-content">
-                      <div className="comment-header">
+                      {/* <div className="comment-header">
                         <span className="nickname">{comment.nickname}</span>
                         <span className="meta">
                           {comment.age}세 · {comment.skinType} ·{" "}
-                          {comment.gender}
+                          {comment.gender}{" "}
+                          {comment.daysAgo && (
+                            <span className="comment-time">
+                              {comment.daysAgo}
+                            </span>
+                          )}
                         </span>
+                      </div> */}
+
+                      <div className="comment-header">
+                        <span className="nickname">{comment.nickname}</span>
+                        <div className="comment-meta-row">
+                          <span className="meta">
+                            {comment.age}세 · {comment.skinType} ·{" "}
+                            {comment.gender}
+                          </span>
+                          {comment.daysAgo && (
+                            <span className="days-ago">{comment.daysAgo}</span>
+                          )}
+                        </div>
                       </div>
+
                       <p className="comment-text">{comment.text}</p>
                     </div>
                   </li>
                 ))}
               </ul>
-              <button className="comment-button" onClick={articleHandleSubmit}>
+              {/* <button className="comment-button" onClick={articleHandleSubmit}>
                 댓글 작성
-              </button>
+              </button> */}
+
+              <div className="article-action-bar">
+                <button
+                  className="comment-submit-button"
+                  onClick={articleHandleSubmit}
+                >
+                  댓글 작성
+                </button>
+
+                <div className="action-icons">
+                  <img
+                    src={article.isLiked ? onHeart : offHeart}
+                    alt="좋아요"
+                    className="action-icon"
+                    onClick={handleToggleLike}
+                  />
+                  {/* <img
+                    src="/icons/bookmark-outline.png"
+                    alt="스크랩"
+                    className="action-icon"
+                    onClick={() => alert("스크랩 준비 중입니다.")}
+                  /> */}
+                </div>
+              </div>
             </div>
           )}
         </div>
